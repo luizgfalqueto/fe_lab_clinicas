ce50ba0c63eea1334989783296cdcad8de59570f
Luiz Gustavo Falqueto <lgfalqueto85@gmail.com>
2024-01-31 16:54:12 -0300

    feat: adding app theme

--- lib/src/lab_clinicas_core_config.dart
+++ lib/src/lab_clinicas_core_config.dart
@@ -1,5 +1,6 @@
 import 'package:asyncstate/asyncstate.dart';
 import 'package:fe_lab_clinicas_core/src/loader/lab_clinicas_loader.dart';
+import 'package:fe_lab_clinicas_core/src/theme/lab_clinicas_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_getit/flutter_getit.dart';
 
@@ -31,6 +32,8 @@ class LabClinicasCoreConfig extends StatelessWidget {
           loader: LabClinicasLoader(),
           builder: (navigatorObserver) {
             return MaterialApp(
+              theme: LabClinicasTheme.lighTheme,
+              darkTheme: LabClinicasTheme.darkTheme,
               navigatorObservers: [
                 flutterGetItNavObserver,
               ],
